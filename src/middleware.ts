f19ebfbb244eef8424ad83d25f316ff775e7dfbc
danielft2 <danielalmeida808z@gmail.com>
2023-04-29 15:30:51 -0300

    refactor: additions to Private Routes Middleware
    Implemented the rule that does not allow the user to return to the login page when logged in.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,20 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
    const token = request.cookies.get('token');
    if (token) {
-      if (request.nextUrl.pathname === '/') {
+      if (
+         request.nextUrl.pathname === '/' ||
+         request.nextUrl.pathname === '/signin'
+      ) {
          return NextResponse.redirect(new URL('/dashboard', request.url));
       }
 
       return NextResponse.next();
    }
 
+   if (request.nextUrl.pathname === '/signin') return NextResponse.next();
    return NextResponse.redirect(new URL('/signin', request.url));
 }
 
 export const config = {
-   matcher: ['/dashboard', '/']
+   matcher: ['/', '/signin', '/dashboard']
 };
