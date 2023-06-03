ef550d87ec6faf467f22bece6e496afce2708fde
danielft2 <danielalmeida808z@gmail.com>
2023-06-03 17:20:54 -0300

    feat: activities menu component tweaks

--- src/app/aplication/subject-details/components/index.ts
+++ src/app/aplication/subject-details/components/index.ts
@@ -1,8 +1,9 @@
 import { Header } from './Header';
-import { ActivityCard } from './AcitivityCard';
-import { InformationsActivity } from './InformationsActivity';
-import { Attachament } from './Attachament';
-import { ShippingDetails } from './ShippingDetails';
+import { ActivityCard } from './Activities/AcitivityCard';
+import { InformationsActivity } from './Activities/InformationsActivity';
+import { Attachament } from './Activities/Attachament';
+import { ShippingDetails } from './Activities/ShippingDetails';
+
 export {
    Header,
    ActivityCard,
